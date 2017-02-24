28e97444c5a495d22f85b8737401e4a99f6e220a
Aleksey Belchenkov <u608110@gmail.com>
2017-02-24 20:57:45 +0300

    Add Jumbotron and Page props

--- js/main.js
+++ js/main.js
@@ -3,11 +3,9 @@ const Theme = React.createClass({
     render: function () {
         return (
             <div>
-                <Navbar />
-                <div className="container">
-                    <Jumbotron />
-                    <Pages />
-                </div>
+                <Navbar brand="React Web Site" currentPage="home" />
+                <Jumbotron heading="Hello, React" text="Приветствую Вас на сайте" btnText="Далее" />
+                <Pages text="This is React Web Site" currentPage="home" />
             </div>
         );
     }
@@ -15,6 +13,14 @@ const Theme = React.createClass({
 
 
 const Navbar = React.createClass({
+
+    getDefaultProps: function () {
+        return {
+            brand: 'Default',
+            currentPage: 'home'
+        }
+    },
+
     render: function () {
         return (
             <nav className="navbar navbar-default">
@@ -26,13 +32,13 @@ const Navbar = React.createClass({
                             <span className="icon-bar"></span>
                             <span className="icon-bar"></span>
                         </button>
-                        <a className="navbar-brand" href="#">Bootstrap theme</a>
+                        <a className="navbar-brand" href="#">{this.props.brand}</a>
                     </div>
                     <div id="navbar" className="navbar-collapse collapse">
                         <ul className="nav navbar-nav">
-                            <li className="active"><a href="#">Home</a></li>
-                            <li><a href="#about">About</a></li>
-                            <li><a href="#contact">Contact</a></li>
+                            <li className={(this.props.currentPage == 'home') ? 'active' : ''}><a href="#">Home</a></li>
+                            <li className={(this.props.currentPage == 'about') ? 'active' : ''}><a href="#about">About</a></li>
+                            <li className={(this.props.currentPage == 'contact') ? 'active' : ''}><a href="#contact">Contact</a></li>
                         </ul>
                     </div>
                 </div>
@@ -44,15 +50,35 @@ const Navbar = React.createClass({
 const Jumbotron = React.createClass({
     render: function () {
         return (
-            <h1>Jumbotron</h1>
+            <div className="jumbotron">
+                <div className="container">
+                    <h1>{this.props.heading}</h1>
+                    <p>{this.props.text}</p>
+                    <p><a className="btn btn-primary btn-lg" href="#" role="button">{this.props.btnText}</a></p>
+                </div>
+            </div>
         );
     }
 });
 
 const Pages = React.createClass({
     render: function () {
+
+        var pageContent;
+
+        if (this.props.currentPage == 'home') {
+            pageContent = 'This is home page';
+        } else if (this.props.currentPage == 'about') {
+            pageContent = 'This is about page';
+        } else if (this.props.currentPage == 'contact') {
+            pageContent = 'This is contact page';
+        }
+
         return (
-            <h1>Pages</h1>
+            <div className="container">
+                <h2>{this.props.currentPage.charAt(0).toUpperCase() + this.props.currentPage.slice(1)}</h2>
+                <p>{pageContent}</p>
+            </div>
         );
     }
 });
